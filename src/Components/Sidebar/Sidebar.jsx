4099cee44c2afce51e2b8bad40cd56a200a2a700
sarbjeetmaan <sarbjeetkaurmaan650@gmail.com>
2025-10-04 04:07:04 -0400

    over

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,23 +1,20 @@
-// src/Components/Sidebar/Sidebar.jsx
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { FaPlusCircle, FaListAlt } from 'react-icons/fa';
 import './Sidebar.css';
+import { Link } from 'react-router-dom';
+import { FaPlusCircle, FaListAlt } from 'react-icons/fa';
 
-const Sidebar = () => {
-  const location = useLocation();
-
+export const Sidebar = () => {
   return (
-    <div className="sidebar">
-      <Link to="addproduct" style={{ textDecoration: 'none' }}>
-        <div className={`sidebar-item ${location.pathname.endsWith('addproduct') ? 'active' : ''}`}>
+    <div className='sidebar'>
+      <Link to='/addproduct' style={{ textDecoration: 'none' }}>
+        <div className="sidebar-item">
           <FaPlusCircle className="sidebar-icon" />
           <p>Add Product</p>
         </div>
       </Link>
 
-      <Link to="listproduct" style={{ textDecoration: 'none' }}>
-        <div className={`sidebar-item ${location.pathname.endsWith('listproduct') ? 'active' : ''}`}>
+      <Link to='/listproduct' style={{ textDecoration: 'none' }}>
+        <div className="sidebar-item">
           <FaListAlt className="sidebar-icon" />
           <p>Product List</p>
         </div>
