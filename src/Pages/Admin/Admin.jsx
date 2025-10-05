9032ff6d774e321211341006dc0d8c6bcb71f9ba
sarbjeetmaan <sarbjeetkaurmaan650@gmail.com>
2025-10-05 01:34:31 -0400

    66

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -4,26 +4,25 @@ import { Routes, Route } from 'react-router-dom';
 import './Admin.css';
 import Sidebar from '../../Components/Sidebar/Sidebar';
 import AddProduct from '../../Components/AddProduct/AddProduct';
-import ListProduct from '../../Components/ListProduct/Listproduct'; 
+import ListProduct from '../../Components/ListProduct/Listproduct';
 import Navbar from '../../Components/Navbar/Navbar';
-import AdminGuard from '../../Guards/AdminGuard';
 
 const Admin = () => {
   return (
-    <AdminGuard>
-      <Navbar /> 
+    <>
+      <Navbar />
       <div className="admin">
         <Sidebar />
         <div className="admin-content">
           <Routes>
             <Route path="addproduct" element={<AddProduct />} />
             <Route path="listproduct" element={<ListProduct />} />
-          
+            {/* ✅ default route so /admin doesn’t look blank */}
             <Route index element={<h2>Welcome to Admin Dashboard</h2>} />
           </Routes>
         </div>
       </div>
-    </AdminGuard>
+    </>
   );
 };
 
