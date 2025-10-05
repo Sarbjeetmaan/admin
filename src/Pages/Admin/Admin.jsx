6eb8a3f7ba8a6e9ce03bb8a0cffbb8341f3d72b8
sarbjeetmaan <sarbjeetkaurmaan650@gmail.com>
2025-10-05 01:30:45 -0400

    55

--- src/Pages/Admin/Admin.jsx
+++ src/Pages/Admin/Admin.jsx
@@ -4,7 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import './Admin.css';
 import Sidebar from '../../Components/Sidebar/Sidebar';
 import AddProduct from '../../Components/AddProduct/AddProduct';
-import ListProduct from '../../Components/ListProduct/ListProduct'; 
+import ListProduct from '../../Components/ListProduct/Listproduct'; 
 import Navbar from '../../Components/Navbar/Navbar';
 import AdminGuard from '../../Guards/AdminGuard';
 
